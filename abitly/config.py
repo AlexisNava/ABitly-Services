5dfae80d55455eafe58bb05e7a2127e311bff205
AlexisNava <brian.alexis.nava.arreola@gmail.com>
2019-09-24 13:30:00 -0500

    Add config.py with Base Configuration and Configuration per environments

---
+++ abitly/config.py
@@ -0,0 +1,25 @@
+"""Define Base Configuration and Configuration per environments"""
+
+from dotenv import load_dotenv
+
+# Exports all the ENV Variables of the .env file
+load_dotenv()
+
+
+class Config:
+    DEBUG = False
+    TESTING = False
+
+
+class ProductionConfig(Config):
+    pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    TESTING = False
+
+
+class TestingConfig(Config):
+    DEBUG = False
+    TESTING = True
